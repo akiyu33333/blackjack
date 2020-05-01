d13fa039e5b49910fbe5e8a1ff99d0db4824cc3d
akiyu33333 <aaaaaki35@gmail.com>
2020-05-01 23:55:12 +0900

    #19 scala : ブラックジャック実装

---
+++ scala/src/main/scala/Card.scala
@@ -0,0 +1,21 @@
+
+class Card(val suit: Suit, val rank: Int) {
+
+  def toDisplayValue(): String = {
+    rank match {
+      case 1 => "A"
+      case 11 => "J"
+      case 12 => "Q"
+      case 13 => "K"
+      case _ => rank.toString
+    }
+  }
+
+  def getPoint(): Int = {
+    if (rank > 10) 10 else rank
+  }
+
+  override def toString: String = {
+    suit.getMark() + "の" + toDisplayValue()
+  }
+}
