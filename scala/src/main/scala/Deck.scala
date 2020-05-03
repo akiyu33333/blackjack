d8ddb7856314006efdee5453efae9e70b6025457
akiyu33333 <aaaaaki35@gmail.com>
2020-05-03 19:27:57 +0900

    S #21 2重ループ、.::の修正

--- scala/src/main/scala/Deck.scala
+++ scala/src/main/scala/Deck.scala
@@ -3,15 +3,11 @@ import scala.util.Random
 case class Deck() {
   var bill = {
     var cards: List[Card] = List()
-    for (s <- Suit.values) {
-      for (i <- 1 to 13) {
-        cards = cards.::(Card(s, i))
-      }
-    }
+    for (s <- Suit.values; i <- 1 to 13) cards = cards :+ Card(s, i)
     Random.shuffle(cards)
   }
 
-  def draw= {
+  def draw = {
     val c = bill.head
     bill = bill.filterNot(_ == c)
     c
